8eb36400bd976f4f4902f608b5aa1ec226d7a281
Santiago Limas <limassantiago000@gmail.com>
2023-05-15 23:14:35 -0500

    Inicio de api Robobank

---
+++ src/main/java/com/proyecto/bank/RobobankApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobobankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/bank/RobobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
